c4e56065c2b1df4ad86a660bc6b5ab5a30a69955
Stephanie McMahon <Smm200973@hotmail.com>
2023-06-30 17:19:49 +0000

    define the views to handle different operations of the booking system

--- restaurant_booking_system/booking/views.py
+++ restaurant_booking_system/booking/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReservationForm
+from .models import Reservation
 
 # Create your views here.
+
+def restaurant_list(request):
+    # Retrieve a list of available restaurants
+    restaurants = Restaurant.objects.all()
+    return render(request, 'booking/restaurant_list.html', {'restaurants': restaurants})
+
+def make_reservation(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking:reservation_success')
+    else:
+        form = ReservationForm()
+    return render(request, 'booking/make_reservation.html', {'form': form})
+
+def reservation_success(request):
+    return render(request, 'booking/reservation_success.html')
