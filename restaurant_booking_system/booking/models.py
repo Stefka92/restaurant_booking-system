c0b26d891dfc1543c76f34527d8588669bbb7c5a
Stephanie McMahon <Smm200973@hotmail.com>
2023-06-30 17:08:50 +0000

    define necessary models for booking system

--- restaurant_booking_system/booking/models.py
+++ restaurant_booking_system/booking/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+Restaurant = models.CharField() 
+Table = models.IntegerField()
+Reservation = models.IntegerField()
+Customer = models.CharField
